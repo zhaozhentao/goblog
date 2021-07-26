a1e2fe3c96d95359e9f6021d1e469929128afb2a
zhaozhentao <qq344696734@gmail.com>
2021-07-26 22:32:40 +0800

    🎨测试about page

--- test/pages_test.go
+++ test/pages_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -16,6 +17,23 @@ func TestHomePage(t *testing.T) {
 		err  error
 	)
 	resp, err = http.Get(baseURL + "/")
+
+	// 2. 检测 —— 是否无错误且 200
+	assert.NoError(t, err, "有错误发生，err 不为空")
+	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+}
+
+func TestAboutPage(t *testing.T) {
+	fmt.Println("TestAboutPage")
+
+	baseURL := "http://localhost:3000"
+
+	// 1. 请求 —— 模拟用户访问浏览器
+	var (
+		resp *http.Response
+		err  error
+	)
+	resp, err = http.Get(baseURL + "/about")
 
 	// 2. 检测 —— 是否无错误且 200
 	assert.NoError(t, err, "有错误发生，err 不为空")
