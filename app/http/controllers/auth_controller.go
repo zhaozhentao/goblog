634017c810dd17d739cc6a401c67d2b7bab0f9d6
zhaozhentao <qq344696734@gmail.com>
2021-07-28 21:29:32 +0800

    用户密码加密

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -40,7 +40,9 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 		_user.Create()
 
 		if _user.ID > 0 {
-			fmt.Fprint(w, "插入成功，ID 为"+_user.GetStringID())
+			// 登录用户并跳转到首页
+			auth.Login(_user)
+			http.Redirect(w, r, "/", http.StatusFound)
 		} else {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(w, "注册失败，请联系管理员")
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/thedevsaddam/govalidator v1.9.10 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	gorm.io/driver/mysql v1.1.1 // indirect
 	gorm.io/gorm v1.21.12 // indirect
 )
